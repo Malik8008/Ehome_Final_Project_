79382703286d9928b7f3c3f0d86070c9c9905001
Malik8008 <tu60prbs1@code.edu.az>
2022-08-30 18:06:07 +0400

    BackUp file Changed

--- Ehome-BackEnd/Controllers/AboutController.cs
+++ Ehome-BackEnd/Controllers/AboutController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ehome_BackEnd.DAL;
+using Ehome_BackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Ehome_BackEnd.Controllers
 {
     public class AboutController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public AboutController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult About()
         {
             return View();
@@ -24,9 +35,10 @@ namespace Ehome_BackEnd.Controllers
             return View();
         }
 
-        public IActionResult Kampaniya()
+        public async Task<IActionResult> Kampaniya()
         {
-            return View();
+            List<Campaing> campaings = await _context.Campaings.ToListAsync(); 
+            return View(campaings);
         }
 
         public IActionResult Blog()
